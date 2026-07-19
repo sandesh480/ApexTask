037a1a4e1021811fca31424f4598fc5b262ab99a
sande-del <sandesharyal617@gmail.com>
2026-07-19 23:10:10 +0545

    Tp fix the bug {DOUBLE SUMBMIT BUG} I applied classic erb design pattern called PRG[POST-REDIRECT-GET]

--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
 
 def task_list(request):
     # Fetch all tasks from our SQLite database
+    if request.method == 'POST':
+        task_title = request.POST.get('title')
+        Task.objects.create(title = task_title)
+        return redirect('task_list')
     all_tasks = Task.objects.all()
     
     # Send them to our HTML template
