03ceb03ebb4cc264e5a7fe7e73163bbe0a1e0f2c
sande-del <sandesharyal617@gmail.com>
2026-07-22 23:17:46 +0545

    update model.py

--- tasks/models.py
+++ tasks/models.py
@@ -10,3 +10,15 @@ class Task(models.Model):
 
     def __str__(self):
         return self.title
+from django.db import models
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+
+    class Meta:
+        # Automatically orders tasks so the newest tasks appear at the top
+        ordering = ['-id']
+
+    def __str__(self):
+        # Tells Django to display the actual title instead of "Task object (1)"
+        return self.title
--- tasks/views.py
+++ tasks/views.py
@@ -5,12 +5,14 @@ from django.shortcuts import render, redirect
 from .models import Task
 
 def task_list(request):
-    # Fetch all tasks from our SQLite database
     if request.method == 'POST':
         task_title = request.POST.get('title')
         Task.objects.create(title = task_title)
         return redirect('task_list')
     all_tasks = Task.objects.all()
-    
-    # Send them to our HTML template
     return render(request, 'tasks/task_list.html', {'tasks': all_tasks})
+
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    return redirect('task_list')
